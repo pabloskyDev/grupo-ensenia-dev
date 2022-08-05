08d44fbef4b9ba76a3045ea480182f189f84a507
Juan Pablo Betancur <jpablobetancur.master2000@gmail.com>
2022-08-05 12:02:55 -0500

    Organizar esqueleto base de las pestañas de la página

---
+++ vista/niveles.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arima&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/css/styles.css">
+    <title>Niveles</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-niveles">
+        <?php include '../templates/header.php';?>
+    </nav>
+
+    <footer class="text-center text-lg-start bg-niveles text-muted">
+        <?php include '../templates/footer.php';?>
+    </footer>
+
+    <?php include '../templates/scripts.php';?>
+    <script src="../src/js/app.js"></script>
+</body>
+</html>
